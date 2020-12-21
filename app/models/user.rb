3a752e366343aa5af97f6a13f6e64ff759e63276
tomas <tomas.stanislovenas@gmail.com>
2020-12-21 15:52:23 +0200

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   rolify
   # Using Devise you can add all of this to database
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :rememberable, :trackable, :validatable
 
   has_many :discussions, dependent: :destroy
 end
